5876272a3c08222ab2683313addc90044bdcb556
John Davison <johncdavison@gmail.com>
2014-08-06 14:09:55 -0700

    Remove smelly config dir.

--- tweetalyzer.rb
+++ tweetalyzer.rb
@@ -4,5 +4,14 @@ require './setup'
 
 # database.rb is where we're defining our DataMapper models
 require './database'
-require './config/twitter_client'
 
+def twitter_client
+  return @client unless @client.nil?
+
+  @client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+    config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+    config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+    config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+  end
+end
